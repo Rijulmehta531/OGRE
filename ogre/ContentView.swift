f5382147ba463ac9945958221bccf8d9a7d00021
Aaron Grizzle <119636980+adgrizzle@users.noreply.github.com>
2023-10-14 22:08:18 -0400

    Initial Commit

---
+++ ogre/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ogre
+//
+//  Created by Aaron Grizzle on 10/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ogre/ogreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ogreApp.swift
+//  ogre
+//
+//  Created by Aaron Grizzle on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ogreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
