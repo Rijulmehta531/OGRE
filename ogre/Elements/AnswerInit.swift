c16fd121c49fedfe90096f1d3baa3b6e841da262
Aaron Grizzle <119636980+adgrizzle@users.noreply.github.com>
2023-10-15 22:28:47 -0400

    Merged all progress on sprint cycle 1.'

---
+++ ogre/Elements/AnswerInit.swift
@@ -0,0 +1,13 @@
+//
+//  Answer.swift
+//  ogre
+//
+//  Created by Rijul Mehta on 10/15/23.
+//
+
+import Foundation
+struct AnswerInit: Identifiable {
+    var id = UUID()
+    var text: AttributedString
+    
+}
--- ogre/ogreApp.swift
+++ ogre/ogreApp.swift
@@ -10,56 +10,26 @@ import Firebase
 import FirebaseAuth
 import FirebaseCore
 
-//// Initialize Firebase
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//    func application(
-//        _ application: UIApplication,
-//        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-//    ) -> Bool {
-//        FirebaseApp.configure()
-//        Auth.auth().useEmulator(withHost: "localhost", port: 9099)
-//        return true
-//    }
-//}
-//
-//
-//@main
-//struct OGRE_PrototypeApp: App {
-//    
-//    // Initialize Firebase
-//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-//    
-//    init() {
-//        
-//    }
-//    var body: some Scene {
-//        WindowGroup {
-//            MainMenuView()
-//                .environment(\.font, Font.custom("Optima", size: 14))
-//        }
-//    }
-//}
-
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-//    Auth.auth().useEmulator(withHost:"localhost", port:9099)
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+//        Auth.auth().useEmulator(withHost:"localhost", port:9099)
+        return true
+    }
 }
 
 @main
 struct ogreApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-  var body: some Scene {
-    WindowGroup {
-      NavigationStack {
-        AuthenticatedView {
-        } content: {
-            MainMenuView()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                AuthenticatedView {
+                } content: {
+                    OnboardingView()
+                }
+            }
         }
-      }
     }
-  }
 }
