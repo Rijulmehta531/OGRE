4bbfd805dceaa61dd00b2247dafef98c78db686b
bjohnson360 <briansjohnson4@gmail.com>
2023-12-03 16:22:27 -0500

    Updated

--- Documents/OGRE/ogre/Google Auth Test/UserProfileView.swift
+++ Documents/OGRE/ogre/Google Auth Test/UserProfileView.swift
@@ -12,8 +12,6 @@ struct UserProfileView: View {
   @EnvironmentObject var viewModel: AuthenticationViewModel
   @Environment(\.dismiss) var dismiss
   @State var presentingConfirmationDialog = false
-    
-    @State var navigateToFriendsView = false
 
   private func deleteAccount() {
     Task {
@@ -27,70 +25,57 @@ struct UserProfileView: View {
     viewModel.signOut()
   }
 
-    var body: some View {
-        NavigationView{
-        Form {
-            Section {
-                VStack {
-                    HStack {
-                        Spacer()
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .frame(width: 100 , height: 100)
-                            .aspectRatio(contentMode: .fit)
-                            .clipShape(Circle())
-                            .clipped()
-                            .padding(4)
-                            .overlay(Circle().stroke(Color.accentColor, lineWidth: 2))
-                        Spacer()
-                    }
-                    Button(action: {}) {
-                        Text("edit")
-                    }
-                }
-            }
-            .listRowBackground(Color(UIColor.systemGroupedBackground))
-            Section("Email") {
-                //Text(viewModel.displayName)
-            }
-            
-            Section("Friends"){
-                NavigationLink(destination: FriendsView()) {
-                                        HStack {
-                                            Spacer()
-                                            Text("Friends")
-                                            Spacer()
-                                        }
-                                    }
-            }
-            
-            Section {
-                Button(role: .cancel, action: signOut) {
-                    HStack {
-                        Spacer()
-                        Text("Sign out")
-                        Spacer()
-                    }
-                }
-            }
-            Section {
-                Button(role: .destructive, action: { presentingConfirmationDialog.toggle() }) {
-                    HStack {
-                        Spacer()
-                        Text("Delete Account")
-                        Spacer()
-                    }
-                }
-            }
+  var body: some View {
+    Form {
+      Section {
+        VStack {
+          HStack {
+            Spacer()
+            Image(systemName: "person.fill")
+              .resizable()
+              .frame(width: 100 , height: 100)
+              .aspectRatio(contentMode: .fit)
+              .clipShape(Circle())
+              .clipped()
+              .padding(4)
+              .overlay(Circle().stroke(Color.accentColor, lineWidth: 2))
+            Spacer()
+          }
+          Button(action: {}) {
+            Text("edit")
+          }
         }
-        .navigationTitle("Profile")
-        .navigationBarTitleDisplayMode(.inline)
-        .analyticsScreen(name: "\(Self.self)")
-        .confirmationDialog("Deleting your account is permanent. Do you want to delete your account?",
-                            isPresented: $presentingConfirmationDialog, titleVisibility: .visible) {
-            Button("Delete Account", role: .destructive, action: deleteAccount)
-            Button("Cancel", role: .cancel, action: { })
+      }
+      .listRowBackground(Color(UIColor.systemGroupedBackground))
+      Section("Email") {
+        //Text(viewModel.displayName)
+      }
+      Section {
+        Button(role: .cancel, action: signOut) {
+          HStack {
+            Spacer()
+            Text("Sign out")
+            Spacer()
+          }
         }
+      }
+      Section {
+        Button(role: .destructive, action: { presentingConfirmationDialog.toggle() }) {
+          HStack {
+            Spacer()
+            Text("Delete Account")
+            Spacer()
+          }
+        }
+      }
+    }
+    .navigationTitle("Profile")
+    .navigationBarTitleDisplayMode(.inline)
+    .analyticsScreen(name: "\(Self.self)")
+    .confirmationDialog("Deleting your account is permanent. Do you want to delete your account?",
+                        isPresented: $presentingConfirmationDialog, titleVisibility: .visible) {
+      Button("Delete Account", role: .destructive, action: deleteAccount)
+      Button("Cancel", role: .cancel, action: { })
     }
   }
 }
