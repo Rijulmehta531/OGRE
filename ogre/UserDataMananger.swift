017f116f374a29bc16ccba8d1591666288d01489
Aaron Grizzle <119636980+adgrizzle@users.noreply.github.com>
2023-12-07 23:04:16 -0500

    Update UserDataMananger.swift
    Added function for initializing and fetching eligible questions and a function to get leaderboard data.

--- ogre/UserDataMananger.swift
+++ ogre/UserDataMananger.swift
@@ -53,14 +53,10 @@ class UserDataManager {
         if let currentUser = Auth.auth().currentUser {
             let userId = currentUser.uid
             let email = currentUser.email ?? ""
-            
-            
-            
             let ref = Database.database().reference().child("all-users")
             ref.observeSingleEvent(of: .value) { snapshot in
                 if let users = snapshot.value as? [String: String] {
                     if !users.keys.contains(userId) {
-                        print("*** Initializing user data.")
                         Database.database().reference().child("all-users").child(userId).setValue(email)
                         writeUserData(userId: userId, element: "dailyStreak", value: 0)
                         writeUserData(userId: userId, element: "tokens", value: 0)
@@ -128,9 +124,29 @@ class UserDataManager {
                             "Vocabulary in Context": ["answeredCorrectly": 0, "attempted": 0]
                         ]
                         writeUserData(userId: userId, element: "subjects", value: subjects)
+                        let eligibleQR: [Int] = Array(0...872)
+                        writeUserData(userId: userId, element: "eligibleQR", value: eligibleQR)
+                        let eligibleVR: [Int] = [23, 32, 37, 80, 99, 101, 102, 107, 112, 126, 142, 151, 168, 170, 176, 197, 199, 201, 218, 220, 241, 259, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519, 520, 521, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549, 550, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611, 612, 613, 614, 615, 616, 617, 618, 619, 620, 621, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 636, 637, 638, 639, 640, 641, 642, 643, 644, 645, 646, 647, 648, 649, 650, 651, 652, 653, 654, 655, 656, 657, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680, 681, 682, 683, 684, 685, 686, 687, 688, 689, 690, 691, 692, 693, 694, 695, 696, 697, 698, 699, 700, 701, 702, 703, 704, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715]
+                        writeUserData(userId: userId, element: "eligibleVR", value: eligibleVR)
                     }
                 }
             }
+        }
+    }
+    
+    static func getEligibleQuestions(category: String, completion: @escaping ([Int]) -> Void) {
+        var element: String
+        if category == "QR" {
+            element = "eligibleQR"
+        } else if category == "VR" {
+            element = "eligibleVR"
+        } else {
+            print("Invalid category")
+            return
+        }
+        readUserData(userId: getUserId(), element: element) { snapshot in
+            let questions = snapshot as? [Int] ?? []
+            completion(questions)
         }
     }
     
@@ -284,6 +300,7 @@ class UserDataManager {
             }
         }
     }
+    
     static func getLeaderboard(completion: @escaping ([String]?) -> Void) {
         var leaderboard: [String] = []
         var usersAndTokens: [String: Int] = [:]
@@ -294,8 +311,6 @@ class UserDataManager {
         readUserData(userId: getUserId(), element: "tokens") { snapshot in
             if let tokens = snapshot as? Int {
                 usersAndTokens[email] = tokens
-                print(email)
-                print(tokens)
                 readUserData(userId: getUserId(), element: "friends") { snapshot in
                     if snapshot is Error {
                         return
@@ -314,14 +329,11 @@ class UserDataManager {
                                         ref.observeSingleEvent(of: .value) { snapshot, _ in
                                             if let tokens = snapshot.value as? Int {
                                                 usersAndTokens[value] = tokens
-                                                print(value)
-                                                print(tokens)
                                             }
                                         }
                                     }
                                 }
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                    print(usersAndTokens)
                                     let sortedArray = usersAndTokens.sorted { $0.value > $1.value }
                                     leaderboard = sortedArray.map { "\($0.key): \($0.value)" }
                                     completion(leaderboard)
